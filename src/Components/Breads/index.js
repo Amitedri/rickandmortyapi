5d95a0e8549c25ac3252e04e591d023993130adb
amitedri <amitedri778@gmail.com>
2023-06-04 13:07:42 +0300

    updates

--- src/Components/Breads/index.js
+++ src/Components/Breads/index.js
@@ -50,7 +50,7 @@ const Breads = () => {
             </span>
           );
         })}
-        {last != false && last.value > currentIndex.value && (
+        {last != false && last.value <= currentIndex.value && (
           <span className={`hoverSoft ${handleItemClass(last, currentIndex)}`} onClick={() => handlePageChange(last)}>
             ... {last.value}
           </span>
